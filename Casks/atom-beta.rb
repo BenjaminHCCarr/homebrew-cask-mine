31478e08c34605dfbba06bc3298b6017efa0531d
Benjamin H C Carr <bencarr+git@gmail.com>
2017-06-23 02:58:03 -0400

    update atom-beta sha, and digikam 5.6.0

---
+++ Casks/atom-beta.rb
@@ -0,0 +1,32 @@
+cask 'atom-beta' do
+  version '1.19.0-beta1'
+  sha256 '3bb135a1ae6ad10fb8bce9f49a2a7b9a50a3fa9e82c1537fcf44ae0e1b631d08'
+
+  # github.com/atom/atom was verified as official when first introduced to the cask
+  url "https://github.com/atom/atom/releases/download/v#{version}/atom-mac.zip"
+  appcast 'https://github.com/atom/atom/releases.atom',
+          checkpoint: 'a41308774b0a3a467420dc1ff6ecd72e0aaa0aee774b551cb4be6dfb83c45d8d'
+  name 'Github Atom Beta'
+  homepage 'https://atom.io/beta'
+
+  auto_updates true
+  depends_on macos: '>= :mountain_lion'
+
+  app 'Atom Beta.app'
+  binary "#{appdir}/Atom Beta.app/Contents/Resources/app/apm/node_modules/.bin/apm", target: 'apm-beta'
+  binary "#{appdir}/Atom Beta.app/Contents/Resources/app/atom.sh", target: 'atom-beta'
+
+  zap delete: [
+                '~/.atom',
+                '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.github.atom.sfl',
+                '~/Library/Application Support/ShipIt_stderr.log',
+                '~/Library/Application Support/Atom',
+                '~/Library/Application Support/ShipIt_stdout.log',
+                '~/Library/Application Support/com.github.atom.ShipIt',
+                '~/Library/Caches/com.github.atom',
+                '~/Library/Caches/com.github.atom.ShipIt',
+                '~/Library/Preferences/com.github.atom.helper.plist',
+                '~/Library/Preferences/com.github.atom.plist',
+                '~/Library/Saved Application State/com.github.atom.savedState',
+              ]
+end
