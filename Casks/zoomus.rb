a448255bcc8b97fad144f4b556f01ea89dc4ca2f
Benjamin H C Carr <bencarr+git@gmail.com>
2018-02-22 23:05:49 -0500

    Add zoom.us client

---
+++ Casks/zoomus.rb
@@ -0,0 +1,26 @@
+cask 'zoomus' do
+  version '4.1.20446.0209'
+  sha256 '87f26cbf531472db043802c97ebb8447459a3e32290199497a6da6a7d9b024f8'
+
+  url "https://www.zoom.us/client/#{version}/zoomusInstaller.pkg"
+  name 'Zoom.us'
+  homepage 'https://www.zoom.us/'
+
+  auto_updates true
+
+  pkg 'zoomusInstaller.pkg'
+
+  uninstall delete: '/Applications/zoom.us.app'
+
+  zap trash: [
+               '~/Desktop/Zoom',
+               '~/Library/Application Support/zoom.us',
+               '~/Library/Caches/us.zoom.xos',
+               '~/Library/Internet Plug-Ins/ZoomUsPlugIn.plugin',
+               '~/Library/Logs/zoom.us',
+               '~/Library/Logs/zoominstall.log',
+               '~/Library/Preferences/ZoomChat.plist',
+               '~/Library/Preferences/us.zoom.xos.plist',
+               '~/Library/Saved Application State/us.zoom.xos.savedState',
+             ]
+end
