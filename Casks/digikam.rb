f8b34c3cf49a5e3a381327b7eb91bb1a17185da4
Benjamin H C Carr <bencarr+git@gmail.com>
2019-02-19 11:54:00 -0600

    Update digikam to 6.0.0

---
+++ Casks/digikam.rb
@@ -0,0 +1,14 @@
+cask 'digikam' do
+  version '6.0.0'
+  sha256 '0cf39ef779876ae98b0c681afa9acab6724725fdab55b208c2a136c946dbdc2f'
+
+  # kde.org/stable/digikam was verified as official when first introduced to the cask
+  url "https://download.kde.org/stable/digikam/#{version}/digiKam-#{version}-MacOS-x86-64.pkg"
+  appcast 'https://download.kde.org/stable/digikam/'
+  name 'digiKam'
+  homepage 'https://www.digikam.org/'
+
+  pkg "digikam-#{version}-MacOS-x86-64.pkg"
+
+  uninstall pkgutil: 'org.freedesktop.digikam'
+end
